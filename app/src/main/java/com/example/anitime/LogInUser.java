b9df182b2bcfe6e3bbb86ec59040f266914d6ba5
Haotian Wang <haotian_wang@hotmail.com>
2022-11-13 00:44:55 -0600

    login & register button now functional, made the login page as the launcher page

--- app/src/main/java/com/example/anitime/LogInUser.java
+++ app/src/main/java/com/example/anitime/LogInUser.java
@@ -2,13 +2,37 @@ package com.example.anitime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LogInUser extends AppCompatActivity {
+public class LogInUser extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_user);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        login = (TextView) findViewById(R.id.login);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+
+            case R.id.login:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
     }
 }
