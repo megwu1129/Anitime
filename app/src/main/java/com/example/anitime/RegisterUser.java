52a06b8ceade4a60eb449c93b1975395fd8397b6
Haotian Wang <haotian_wang@hotmail.com>
2022-11-13 01:29:45 -0600

    Added some registration verification stuff

--- app/src/main/java/com/example/anitime/RegisterUser.java
+++ app/src/main/java/com/example/anitime/RegisterUser.java
@@ -1,30 +1,102 @@
 package com.example.anitime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
     private TextView register;
+    private EditText editTextFullName, editTextPhone, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
 
+        mAuth = FirebaseAuth.getInstance();
+
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.FullName);
+        editTextPhone = (EditText) findViewById(R.id.Phone);
+        editTextEmail = (EditText) findViewById(R.id.Email);
+        editTextPassword = (EditText) findViewById(R.id.Password);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.register:
-                startActivity(new Intent(this, LogInUser.class));
+                registerUser();
+                //startActivity(new Intent(this, LogInUser.class));
                 break;
         }
+    }
+    public void registerUser(){
+        String name = editTextFullName.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(name.isEmpty()){
+            editTextFullName.setError("Please enter you full name.");
+            editTextFullName.requestFocus();
+            return;
+        }
+        if(phone.isEmpty()){
+            editTextPhone.setError("Please enter you phone number.");
+            editTextPhone.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            editTextEmail.setError("Please enter you email.");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Email is not in a valid format.");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Please enter you password.");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPassword.setError("Minimum password length should be 6 characters.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        Toast.makeText(RegisterUser.this, "You are registered successfully!", Toast.LENGTH_LONG).show();
+
+//        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if(task.isSuccessful()){
+//                    User user = new User(name, phone, email);
+//                    FirebaseAuth.
+//                }
+//            }
+//        })
     }
 }
---
+++ app/src/main/java/com/example/anitime/User.java
@@ -0,0 +1,15 @@
+package com.example.anitime;
+
+public class User {
+    public String fullname, phone, email;
+
+    public User(){
+
+    }
+
+    public User(String fullname, String phone, String email){
+        this.fullname = fullname;
+        this.phone = phone;
+        this.email = email;
+    }
+}
