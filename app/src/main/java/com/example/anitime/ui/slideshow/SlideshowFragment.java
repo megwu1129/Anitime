899769a769b38a33efda7cb13cab661e15997a7a
Meg Wu <73263355+megwu1129@users.noreply.github.com>
2022-11-27 20:47:29 -0600

    revise recyclerview

--- app/src/main/java/com/example/anitime/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/anitime/ui/slideshow/SlideshowFragment.java
@@ -1,5 +1,6 @@
 package com.example.anitime.ui.slideshow;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,28 +10,50 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.anitime.Adapters.EventsRecyclerViewAdapter;
 import com.example.anitime.EventActivity;
 import com.example.anitime.R;
 import com.example.anitime.RegisterUser;
+import com.example.anitime.Repository.EventsRepo;
 
-public class SlideshowFragment extends Fragment implements View.OnClickListener {
+//public class SlideshowFragment extends Fragment implements View.OnClickListener {
+//
+//    private ImageView eventDetailButton;
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.fragment_events, container, false);
+//        eventDetailButton = (ImageView) rootView.findViewById(R.id.card_image1);
+//        eventDetailButton.setOnClickListener(this);
+//        return rootView;
+//    }
+//
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.card_image1) {
+//            Intent intent = new Intent(v.getContext(), EventActivity.class);
+//            startActivity(intent);
+//        }
+//    }
+//}
 
-    private ImageView eventDetailButton;
-
+public class SlideshowFragment extends Fragment  {
+    RecyclerView recyclerView;
+    EventsRecyclerViewAdapter eventsRecyclerViewAdapter;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_events, container, false);
-        eventDetailButton = (ImageView) rootView.findViewById(R.id.card_image1);
-        eventDetailButton.setOnClickListener(this);
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        initRecyclerView();
         return rootView;
     }
-
-    public void onClick(View v) {
-        if (v.getId() == R.id.card_image1) {
-            Intent intent = new Intent(v.getContext(), EventActivity.class);
-            startActivity(intent);
-        }
+    void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false);
+        eventsRecyclerViewAdapter = new EventsRecyclerViewAdapter(EventsRepo.getEventsRepo().getEventModelList());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(eventsRecyclerViewAdapter);
     }
 }
