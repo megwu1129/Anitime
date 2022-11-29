7b42986aeedbe377a8fc0b65cb7f5260a3e5992c
ZhuoruLi <douglashu0822@163.com>
2022-11-28 18:22:11 -0600

    glide and pet recyclerView
    The app starts to crash, need to discuss why

---
+++ app/src/main/java/com/example/anitime/ui/home/PetsProfile.java
@@ -0,0 +1,91 @@
+package com.example.anitime.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.anitime.Adapters.PetsRecyclerViewAdapter;
+import com.example.anitime.Pet_details;
+import com.example.anitime.R;
+import com.example.anitime.RegisterUser;
+import com.example.anitime.Repository.PetsRepo;
+//import com.example.anitime.databinding.FragmentHomeBinding;
+
+//public class HomeFragment extends Fragment  {
+//
+//    RecyclerView recyclerView;
+////    private ImageView peekButton;
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.fragment_pet_profiles, container, false);
+//        peekButton = (ImageView) rootView.findViewById(R.id.card_image3);
+//        peekButton.setOnClickListener(this);
+//        return rootView;
+//    }
+
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.card_image3) {
+//            Intent intent = new Intent(v.getContext(),Pet_details.class);
+//            startActivity(intent);
+//        }
+//    }
+
+//    private FragmentHomeBinding binding;
+//
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
+//        HomeViewModel homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
+//
+//        binding = FragmentHomeBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final NestedScrollView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
+//    }
+//
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
+//}
+public class PetsProfile extends AppCompatActivity {
+    RecyclerView recyclerView;
+    PetsRecyclerViewAdapter petsRecyclerViewAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_pet_profiles);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        initRecyclerView();
+    }
+    void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        petsRecyclerViewAdapter = new PetsRecyclerViewAdapter(PetsRepo.getPetsRepo().getPetsModelList());
+        Log.i("data-->",""+PetsRepo.getPetsRepo().getPetsModelList().size());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(petsRecyclerViewAdapter);
+        petsRecyclerViewAdapter.notifyDataSetChanged();
+
+    }
+}
