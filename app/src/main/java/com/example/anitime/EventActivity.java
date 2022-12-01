26399afb54078b5ddbbdae738b249b7e9e2e171f
Haotian Wang <haotian_wang@hotmail.com>
2022-11-30 19:00:20 -0600

    Update EventActivity.java

--- app/src/main/java/com/example/anitime/EventActivity.java
+++ app/src/main/java/com/example/anitime/EventActivity.java
@@ -1,19 +1,43 @@
 package com.example.anitime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.anitime.Adapters.EventsRecyclerViewAdapter;
+import com.example.anitime.Models.EventModel;
 
-public class EventActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.event_detail);
-//    }
+public class EventActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView button;
+    ArrayList<EventModel> eventModelArrayList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_detail);
-}
+
+        button = (TextView) findViewById(R.id.attendButton);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.attendButton:
+                addAttendee();
+                break;
+        }
+    }
+
+    public void addAttendee() {
+        Toast.makeText(this, "You have been added to the list of attendee!",
+                Toast.LENGTH_SHORT).show();
+
+        System.out.println(eventModelArrayList.size());
+    }
 }
