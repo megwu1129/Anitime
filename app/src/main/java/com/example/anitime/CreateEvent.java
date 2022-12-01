881542c37ddf7fbcca38144956386a63aee0f06b
Haotian Wang <haotian_wang@hotmail.com>
2022-11-30 18:46:01 -0600

    added "create event" tab

---
+++ app/src/main/java/com/example/anitime/CreateEvent.java
@@ -0,0 +1,22 @@
+package com.example.anitime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class CreateEvent extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_event);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
