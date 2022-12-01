6115e7658f75797093fce516866e35d8259d8238
Meg Wu <73263355+megwu1129@users.noreply.github.com>
2022-11-30 18:48:44 -0600

    add 3 event detail pages

--- app/src/main/java/com/example/anitime/EventActivity.java
+++ app/src/main/java/com/example/anitime/EventActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class EventActivity extends AppCompatActivity {
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.event_detail);
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_detail);
-    }
+}
 }
