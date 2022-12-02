8b57bb7bc449a7e8402c524bae1ad561b503b902
shreyahprasad <sprasa20@illinois.edu>
2022-12-02 03:07:45 -0600

    create event done

--- app/src/main/java/com/example/anitime/CreateEvent.java
+++ app/src/main/java/com/example/anitime/CreateEvent.java
@@ -1,18 +1,91 @@
 package com.example.anitime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.anitime.Repository.EventsRepo;
+import com.example.anitime.databinding.FragmentYourProfileBinding;
+import com.example.anitime.ui.gallery.GalleryViewModel;
 
 public class CreateEvent extends AppCompatActivity implements View.OnClickListener{
+
+    private Button editInfo;
+    private Button createInfo;
+    LinearLayout displayEvent, createEvent;
+    EditText editTextEventName, editTextMonth, editTextDate, editTextEventTime,
+            editTextEventLoc, editTextEventDesc;
+    TextView disEventName, disEventDate, disEventTime, disEventLoc, disEventDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event);
+
+        editInfo = (Button) findViewById(R.id.editEventButton);
+        createInfo = (Button) findViewById(R.id.createEventButton);
+        createInfo.setVisibility(View.GONE);
+
+        displayEvent = (LinearLayout) findViewById(R.id.displayEvent);
+        createEvent = (LinearLayout) findViewById(R.id.createEvent);
+        createEvent.setVisibility(View.GONE);
+
+        disEventName = (TextView) findViewById(R.id.disEventName);
+        disEventDate = (TextView) findViewById(R.id.disEventDate);
+        disEventTime = (TextView) findViewById(R.id.disEventTime);
+        disEventLoc = (TextView) findViewById(R.id.disEventLoc);
+        disEventDesc = (TextView) findViewById(R.id.disEventDesc);
+
+        editInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editInfo.setVisibility(View.GONE);
+                createInfo.setVisibility(View.VISIBLE);
+
+                displayEvent.setVisibility(View.GONE);
+                createEvent.setVisibility(View.VISIBLE);
+            }
+        });
+
+        createInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextEventName = (EditText) findViewById(R.id.editTextEventName);
+                editTextMonth = (EditText) findViewById(R.id.editTextMonth);
+                editTextDate = (EditText) findViewById(R.id.editTextDate);
+                editTextEventTime = (EditText) findViewById(R.id.editTextEventTime);
+                editTextEventLoc = (EditText) findViewById(R.id.editTextEventLoc);
+                editTextEventDesc = (EditText) findViewById(R.id.editTextEventDesc);
+
+                disEventName.setText(editTextEventName.getText().toString());
+                disEventDate.setText(editTextMonth.getText().toString() + " " + editTextDate.getText().toString());
+                disEventTime.setText(editTextEventTime.getText().toString());
+                disEventLoc.setText(editTextEventLoc.getText().toString());
+                disEventDesc.setText(editTextEventDesc.getText().toString());
+
+                editInfo.setVisibility(View.VISIBLE);
+                createInfo.setVisibility(View.GONE);
+
+                displayEvent.setVisibility(View.VISIBLE);
+                createEvent.setVisibility(View.GONE);
+
+                EventsRepo.addEvent(disEventName.getText().toString(), editTextMonth.getText().toString(), editTextDate.getText().toString(), disEventLoc.getText().toString());
+                EventsRepo.printEvents();
+
+            }
+        });
+
+
     }
 
     @Override
--- app/src/main/java/com/example/anitime/Repository/EventsRepo.java
+++ app/src/main/java/com/example/anitime/Repository/EventsRepo.java
@@ -1,18 +1,19 @@
 package com.example.anitime.Repository;
 
 import com.example.anitime.Models.EventModel;
+import com.example.anitime.Models.PetsModel;
 import com.example.anitime.R;
 
 import java.util.ArrayList;
 
 public class EventsRepo {
     private static EventsRepo eventsRepo;
-    private ArrayList<EventModel> eventModelList = new ArrayList<>();
+    private static ArrayList<EventModel> eventModelList = new ArrayList<>();
 
     public EventsRepo(){
         eventModelList.add(new EventModel("29", "NOV", "Puppy Social", "Helms Park, Champaign", "12", R.drawable.event3));
         eventModelList.add(new EventModel("2", "DEC", "Dogs Walking", "Main Quad, UIUC", "6", R.drawable.event5));
-        eventModelList.add(new EventModel("15", "JAN", "1:1 Dog Training Class", "Online", "1", R.drawable.event1));
+        eventModelList.add(new EventModel("15", "JAN", "Dog Training Class", "Online", "1", R.drawable.event1));
         eventModelList.add(new EventModel("4", "FEB", "Meeting New Friends!", "Pet-Friendly Restaurant", "27", R.drawable.event2));
     }
 
@@ -22,7 +23,21 @@ public class EventsRepo {
         }
         return eventsRepo;
     }
-    public ArrayList<EventModel> getEventModelList() {
+
+    public static void addEvent(String eventName, String month, String day, String location) {
+        eventModelList.add(new EventModel(day, month, eventName, location, "1", R.drawable.event1));
+
+    }
+
+    public static void printEvents() {
+        for (int i = 0; i < eventModelList.size(); i++) {
+            EventModel p = eventModelList.get(i);
+            System.out.println(p.getTitle());
+        }
+
+    }
+
+    public static ArrayList<EventModel> getEventModelList() {
         return eventModelList;
     }
 }
